4c07d06528287fefdd1157e5548bc503b4ec654d
Martin R <martin.ristvedt@gmail.com>
2024-04-29 12:32:04 +0200

    nytt css&js fil røft oppsett av artworkprofile View m filer lagt til i index

---
+++ singleArtworkpieceProfileView.js
@@ -0,0 +1,10 @@
+function updateSingleArtworkpieceProfileView(){
+    document.getElementById("appingtonTheSecond").innerHTML = /*HTML*/`
+    <div class="artworkProfile_Container">
+        <div class="item section_1">Én</div>
+        <div class="item section_2">To</div>
+        <div class="item section_3">Tre</div>
+        <div class="item section_4">Fire</div>
+    </div>
+    `;
+}
