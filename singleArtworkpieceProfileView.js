64aca3d07c79c17eb86b8d09d767d4fbf0b4682c
Martin R <martin.ristvedt@gmail.com>
2024-04-30 02:36:57 +0200

    further battles at the Canadian Sinclair Scouts (singular artwork view)

--- singleArtworkpieceProfileView.js
+++ singleArtworkpieceProfileView.js
@@ -1,18 +1,64 @@
 function updateSingleArtworkpieceProfileView(){
     document.getElementById("appingtonTheSecond").innerHTML = /*HTML*/`
     <div class="artworkProfile_Container">
-        <div class="item section_1">Én</div>
-        <div class="item section_2">To</div>
-        <div class="item section_3">Tre</div>
-        <div class="item section_4">Fire</div>
-        <div class="item section_5">Fem</div>
-        <div class="item section_6">Seks</div>
-        <div class="item section_7">Syv</div>
-        <div class="item section_8">Åtte</div>
-        <div class="item section_9">Ni</div>
-        <div class="item section_10">Ti</div>
-        <div class="item section_11">Elleve</div>
-        <div class="item section_12">Tolv</div>
+        <div class="item section_1">1</div>
+        
+        <div class="section_2"></div>
+
+        <div class="item section_3">
+            <div class="s3_a"></div>
+            <div class="s3_b"></div>
+            <div class="s3_c"></div>
+            <div class="item s3_d">d</div>
+            <div class="s3_e"></div>
+            <div class="item s3_f">f</div>
+            <div class="s3_g"></div>
+            <div class="item s3_h">h</div>
+            <div class="item s3_i">i</div>
+        </div>
+
+        <div class="item section_4">4</div>
+        <div class="item section_5">5</div>
+        <div class="item section_6">6</div>
+
+        <div class="item section_7">
+            <div class="s7_d">
+                <div>xx:xx Tog tåget ner, du skulle 1000kr</div>
+                <div>xx:xx dumpa mig B'attre på 1100kr</div>
+                <div>xx:xx plats än genom telefon 1200kr</div>
+                <div>xx:xx Jag visste inte vad som 1300kr</div>
+                <div>xx:xx väntade Första 1500kr</div>
+                <div>xx:xx semesteren på fyra år  1700kr</div>
+                <div>xx:xx Som jag förstod var vi 2000kr</div>
+                <div>xx:xx förälskade Du sa att 2350kr</div>
+                <div><i>Minste bud: 2700kr</i></div>
+            </div>
+        </div>
+        <div class="section_8"></div>
+        <div class="item section_9">9 - Lorem joej fdf jg kjkf fhi fh jd fihfdkh fdf dff df fdfjf hfk f sd dfhf bhvhv vhuf jfnf jkfvjkjvkkkg gjgglgg fklkgg dkdjllkgg kgfgk jggkggkjg</div>
+
+        <div class="section_10"></div>
+        
+        <div class="section_11"></div>
+        <div class="item section_12">
+            <p class="s12_head">12</p>
+            <p class="s12_p">
+                12 - Lorem joej fdf jg kjkf fhi fh jd fihfdkh fdf
+                dff df fdfjf hfk f sd dfhf bhvhv vhuf jfnf jkfvjkjvkkkg
+                gjgglgg fklkgg dkdjllkgg kgfgk jggkggkjg Lorem joej fdf
+                jg kjkf fhi fh jd fihfdkh fdf dff df fdfjf hfk f sd dfhf
+                bhvhv vhuf jfnf jkfvjkjvkkkg jgglgg fklkgg dkdjllkgg
+                kgfgk jggkggkjg Lorem kjkf fhi fh jd fihfdkh fdf
+                dff df fdfjf hfk f sd dfhf bhvhv vhuf jfnf jkfvjkjvkkkg
+                gjgglgg fklkgg dkdjllkgg kgfgk jggkggkjg Lorem joej fdf
+                jg kjkf fhi fh jd fihfdkh fdf dff df fdfjf hfk f sd dfhf
+                bhvhv vhuf jfnf jkfvjkjvkkkg jgglgg fklkgg dkdjllkgg
+                kgfgk jggkggkjg
+            </p>
+        </div>
+        <div class="section_13"></div>
+
+        <div class="section_14"></div>
     </div>
     `;
 }
